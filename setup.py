26a3df962a3403ef69245558e8dc4a447b853651
subsurfaceiodev <109676812+subsurfaceiodev@users.noreply.github.com>
2024-07-26 15:19:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-from gmspy.__about__ import __version__
+from gmspy.version import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
